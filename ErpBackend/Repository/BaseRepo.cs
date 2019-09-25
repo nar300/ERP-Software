1a93e618a9c8544dcef760bcd126d83581fefb65
nar300 <ni781233@gmail.com>
2019-09-25 12:13:30 +0100

    Employee Repository Added

---
+++ ErpBackend/Repository/BaseRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ErpBackend.Repository
+{
+    public interface BaseRepo <T> where T:class
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> Create(T body);
+        Task<T> GetById(int id);
+        Task<T> Delete(T body);
+        Task<T> Update(T body, int id);
+        Task<bool> IsSaved();
+
+    }
+}
---
+++ ErpBackend/Repository/EmployeeRepoImpl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ErpBackend.Data;
+using ErpBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpBackend.Repository
+{
+    public class EmployeeRepoImpl : EmployeeRepo
+    {
+       private readonly ErpDbContext _db;
+        public EmployeeRepoImpl(ErpDbContext db)
+        {
+            _db = db;
+        }
+        public async Task< Employee> Create(Employee body)
+        {
+            _db.Employees.Add(body);
+            await _db.SaveChangesAsync();
+            return body;
+        }
+
+        public async Task<Employee> Delete(Employee body)
+        {
+            _db.Employees.Remove(body);
+            await _db.SaveChangesAsync();
+            return body;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAll()
+        {
+           var emplist = await _db.Employees.ToListAsync();
+            return emplist;
+        }
+
+        public async Task<Employee> GetById(int id)
+        {
+           var Emp= await _db.Employees.FirstOrDefaultAsync(emp => emp.Id == id);
+            return Emp;
+        }
+
+        public async Task<bool> IsSaved()
+        {
+           
+            return  await _db.SaveChangesAsync()>0;
+        }
+
+        public async Task<Employee> Update(Employee body, int id)
+        {
+
+            _db.Entry(body).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return body;
+        }
+    }
+}
